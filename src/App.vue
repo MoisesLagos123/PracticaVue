b8b77ed04ebd6afc9d67e25a1702d43da4a64952
MoisesLagos123 <moises.lagos.f@gmail.com>
2022-01-18 18:23:26 -0300

    primer guardado

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TablaBurger></TablaBurger>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TablaBurger from './components/TablaBurger.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TablaBurger
   }
 }
 </script>
---
+++ src/components/TablaBurger.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <b-card title="MENU" class="ml-5 mr-5 mb-5">
+            <b-button variant="success" class="mb-4">AGREGAR</b-button>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>NRO</th>
+                        <th>DESCRIPCION</th>
+                        <th>CALORIAS</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="todo in todos" :key="todo.id">
+                        <td>{{todo.id}}</td>
+                        <td>{{todo.nombre}}</td>
+                        <td>
+                            <b-button variant="dark" class="mr-2">VER DETALES</b-button>
+                            <b-button variant="warning" class="mr-2">EDITAR</b-button>
+                            <b-button variant="danger" >ELIMINAR</b-button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>  
+        </b-card>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default{
+    data(){
+        return{
+            todos:null
+        }
+    },
+    mounted(){
+        this.getTodos();
+    },
+    methods: {
+        getTodos(){
+            axios
+                .get('https://prueba-hamburguesas.herokuapp.com/burger/')
+                .then(response => {
+                    console.log(response)
+                    this.todos = response.data
+                })
+                .catch(e=> console.log(e))
+        }
+    }
+    
+}
+</script>
